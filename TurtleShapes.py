cd797c3f3a21117ada02d03750c2cbd4e8d409b9
Cometzfordayz20 <63605087+Cometzfordayz20@users.noreply.github.com>
2023-08-29 21:32:22 -0600

    Update TurtleShapes.py

--- TurtleShapes.py
+++ TurtleShapes.py
@@ -3,11 +3,14 @@ import math
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import turtle
+# Controlls The size of every polygon shape
 size = 50
-
+# Controlls number of polygons after square
 numOfPolygons = 4
-s = turtle.getscreen()
 
+# Turtle Graphics Screen object
+s = turtle.getscreen()
+# Turtle Graphics Turtle object
 t = turtle.Turtle()
 
 t.penup()
@@ -30,8 +33,9 @@ t.rt(90)
 t.fd(size)
 
 for n in range(5, numOfPolygons + 4):
-    currentAngle = ((n-2)*180)/n
-    lastAngle = ((n-3)*180)/(n-1)
+    
+    currentAngle = ((n-2)*180)/n    # Derived from the equation to represent the interior angle of any polygon with sides n
+    lastAngle = ((n-3)*180)/(n-1)   # Similar to CurrentAngle but gets the interior angle of the last polygon allowing us to compute turn angles 
     switch = currentAngle-lastAngle
     turnDeg = 180 - currentAngle
     if math.fmod(n, 2) != 0:
